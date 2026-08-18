617a3a93970f3e71b865470d8f29afa02829f1c2
Fentomi <105577819+Fentomi@users.noreply.github.com>
2026-08-18 14:06:52 +0300

    Рефакторинг: удаляет класс SecureData и использует .env для загрузки данных окружения.

--- RestAssured/src/test/java/practice/restassured/secure/SecureData.java
+++
@@ -1,8 +0,0 @@
-package practice.restassured.secure;
-
-import lombok.Getter;
-
-public class SecureData {
-  private @Getter static String token = "perm-YWRtaW4=.NDItMA==.O11eKvKsqveipnwIcL5nfIg9QaaGc7";
-  private @Getter static String URI = "http://localhost:8080/api";
-}
--- RestAssured/src/test/java/practice/restassured/specifications/Specifications.java
+++ RestAssured/src/test/java/practice/restassured/specifications/Specifications.java
@@ -5,15 +5,16 @@ import io.restassured.builder.ResponseSpecBuilder;
 import io.restassured.filter.log.LogDetail;
 import io.restassured.specification.RequestSpecification;
 import io.restassured.specification.ResponseSpecification;
-import practice.restassured.secure.SecureData;
+import io.github.cdimascio.dotenv.Dotenv;
 
 public class Specifications {
+  private static final Dotenv dotenv = Dotenv.load();
 
   public static RequestSpecification reqSpec() {
     return new RequestSpecBuilder()
-        .setBaseUri(SecureData.getURI())
+        .setBaseUri(dotenv.get("API_URI"))
         .log(LogDetail.ALL)
-        .addHeader("Authorization", "Bearer " + SecureData.getToken())
+        .addHeader("Authorization", "Bearer " + dotenv.get("API_TOKEN"))
         .addHeader("Content-Type", "application/json")
         .build();
   }
